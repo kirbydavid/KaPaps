5489c068eabfdd5914e93d6dfad43036d5124f26
Franz <f3202725@gmail.com>
2025-01-12 07:50:30 +0800

    di mapagana

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -2,21 +2,20 @@
 
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBgH5mHABTUava3rf738IGQFiZ6jIZ4Ww0",
-  authDomain: "plm-hackathon.firebaseapp.com",
-  databaseURL: "https://plm-hackathon-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "plm-hackathon",
-  storageBucket: "plm-hackathon.firebasestorage.app",
-  messagingSenderId: "1041012615777",
-  appId: "1:1041012615777:web:8d8799e1a15cb506725820",
-  measurementId: "G-FBQNN0LH76"
+   apiKey: "AIzaSyBgH5mHABTUava3rf738IGQFiZ6jIZ4Ww0",
+   authDomain: "plm-hackathon.firebaseapp.com",
+   databaseURL: "https://plm-hackathon-default-rtdb.asia-southeast1.firebasedatabase.app",
+   projectId: "plm-hackathon",
+   storageBucket: "plm-hackathon.firebasestorage.app",
+   messagingSenderId: "1041012615777",
+   appId: "1:1041012615777:web:8d8799e1a15cb506725820",
+   measurementId: "G-FBQNN0LH76"
 };
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
